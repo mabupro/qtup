2cbb249e08c2361591f91e228d79da454633e2ef
mabupro <mabupro787@gmail.com>
2025-01-27 11:44:14 +0900

    Prettier を設定

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import { HelloWorldLabel } from '@/components/hello-world-label';
+import { HelloWorldLabel } from "@/components/hello-world-label";
 
 export default function Home() {
   return (
     <main>
-      <div className="flex justify-center items-center h-screen">
+      <div className="flex h-screen items-center justify-center">
         <div className="flex flex-col items-center">
           <h1 className="text-2xl font-bold">Hello Galaxy</h1>
           <HelloWorldLabel />
